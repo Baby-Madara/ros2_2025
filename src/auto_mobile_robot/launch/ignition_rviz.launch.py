e033637135f8ec6c825cb1d586a3878981d3e55f
Baby-Madara <ahmedfarahat1177@gmail.com>
2025-01-22 20:15:28 +0200

    camera solvedagain, next: rgbd cam to solve

--- src/auto_mobile_robot/launch/ignition_rviz.launch.py
+++ src/auto_mobile_robot/launch/ignition_rviz.launch.py
@@ -9,32 +9,27 @@ from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-
-# source /usr/share/gazebo/setup.sh
-
 def generate_launch_description():
 
-    # Set the path to different files and folders.
-    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
-    pkg_share = FindPackageShare(package='auto_mobile_robot').find('auto_mobile_robot')
-    default_launch_dir = os.path.join(pkg_share, 'launch')
-    default_model_path = os.path.join(pkg_share, 'models/auto_mobile_robot.urdf')
-    robot_localization_file_path = os.path.join(pkg_share, 'config/ekf.yaml') 
-    robot_name_in_urdf = 'auto_mobile_robot'
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-    # world_file_name = 'house.sdf'
-    world_file_name = 'smalltown.world'
-    world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+    # Set the path to different files and folders
+    pkg_share                     = FindPackageShare(package='auto_mobile_robot').find('auto_mobile_robot')
+    default_launch_dir            = os.path.join(pkg_share, 'launch')
+    default_model_path            = os.path.join(pkg_share, 'models/auto_mobile_robot.urdf')
+    robot_localization_file_path  = os.path.join(pkg_share, 'config/ekf.yaml') 
+    robot_name_in_urdf            = 'auto_mobile_robot'
+    default_rviz_config_path      = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    world_file_name               = 'smalltown.world'  # Ignition uses .SDF format for world files
+    world_path                    = os.path.join(pkg_share, 'worlds', world_file_name)
 
     # Launch configuration variables specific to simulation
-    headless = LaunchConfiguration('headless')
-    model = LaunchConfiguration('model')
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
-    use_rviz = LaunchConfiguration('use_rviz')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    use_simulator = LaunchConfiguration('use_simulator')
-    world = LaunchConfiguration('world')
+    headless              = LaunchConfiguration('headless')
+    model                 = LaunchConfiguration('model')
+    rviz_config_file      = LaunchConfiguration('rviz_config_file')
+    use_robot_state_pub   = LaunchConfiguration('use_robot_state_pub')
+    use_rviz              = LaunchConfiguration('use_rviz')
+    use_sim_time          = LaunchConfiguration('use_sim_time')
+    use_simulator         = LaunchConfiguration('use_simulator')
+    world                 = LaunchConfiguration('world')
 
     # Declare the launch arguments  
     declare_model_path_cmd = DeclareLaunchArgument(
@@ -50,7 +45,7 @@ def generate_launch_description():
     declare_simulator_cmd = DeclareLaunchArgument(
         name='headless',
         default_value='False',
-        description='Whether to execute gzclient')
+        description='Whether to execute Ignition Gazebo GUI')
 
     declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
         name='use_robot_state_pub',
@@ -65,7 +60,7 @@ def generate_launch_description():
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         name='use_sim_time',
         default_value='True',
-        description='Use simulation (Gazebo) clock if true')
+        description='Use simulation (Ignition Gazebo) clock if true')
 
     declare_use_simulator_cmd = DeclareLaunchArgument(
         name='use_simulator',
@@ -79,17 +74,20 @@ def generate_launch_description():
 
     # Specify the actions
 
-    # Start Gazebo server
-    start_gazebo_server_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-        condition=IfCondition(use_simulator),
-        # launch_arguments={'world': world}.items()
-        )
+    start_ignition_server_cmd = Node(
+        package='ros_ign_gazebo',
+        executable='ign',
+        output='screen',
+        arguments=['gazebo', '-r', LaunchConfiguration('world')],
+        condition=IfCondition(LaunchConfiguration('use_simulator')))
 
-    # Start Gazebo client    
-    start_gazebo_client_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
-        condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
+    start_ignition_client_cmd = Node(
+        package='ros_ign_gazebo',
+        executable='ign',
+        output='screen',
+        arguments=['gazebo', '-r', LaunchConfiguration('world')],
+        condition=UnlessCondition(LaunchConfiguration('headless')))
+
 
     # Start robot localization using an Extended Kalman filter
     start_robot_localization_cmd = Node(
@@ -100,7 +98,7 @@ def generate_launch_description():
         parameters=[robot_localization_file_path, 
         {'use_sim_time': use_sim_time}])
 
-    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    # Subscribe to the joint states of the robot, and publish the 3D pose of each link
     start_robot_state_publisher_cmd = Node(
         condition=IfCondition(use_robot_state_pub),
         package='robot_state_publisher',
@@ -117,19 +115,7 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', rviz_config_file])
-    
-    
-    
-    
-    # Ensure Gazebo setup is sourced
-    gazebo_setup_path = "/usr/share/gazebo/setup.sh"
-    if os.path.exists(gazebo_setup_path):
-        os.system(f"source {gazebo_setup_path}")
-    else:
-        raise FileNotFoundError(f"Gazebo setup script not found at {gazebo_setup_path}")
-    
-    
-    
+
     # Create the launch description and populate
     ld = LaunchDescription()
 
@@ -144,8 +130,8 @@ def generate_launch_description():
     ld.add_action(declare_world_cmd)
 
     # Add any actions
-    ld.add_action(start_gazebo_server_cmd)
-    ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(start_ignition_server_cmd)
+    ld.add_action(start_ignition_client_cmd)
     ld.add_action(start_robot_localization_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(start_rviz_cmd)
