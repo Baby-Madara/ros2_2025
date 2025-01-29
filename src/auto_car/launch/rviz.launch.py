eaa54e1775d779641af9f6c74c17e751fea2ebd1
Baby-Madara <ahmedfarahat1177@gmail.com>
2025-01-29 21:35:22 +0200

    better robot

--- src/auto_car/launch/rviz.launch.py
+++ src/auto_car/launch/rviz.launch.py
@@ -12,16 +12,18 @@ from launch.substitutions               import Command
 from launch_ros.actions                 import Node
 from launch_ros.substitutions           import FindPackageShare
 from launch.substitutions               import Command, LaunchConfiguration, PythonExpression
+import xacro
 
 
 def generate_launch_description():
     # Set paths
     pkg_ign_gazebo_ros              = FindPackageShare(package='ros_gz_sim').find('ros_gz_sim')  # Ignition package
     pkg_share                       = FindPackageShare(package='auto_car').find('auto_car')
-    model_path                      = os.path.join(pkg_share, 'models/auto_mobile_robot.urdf')
+    model_path                      = os.path.join(pkg_share, 'urdf/car.urdf.xacro')
     rviz_config_path                = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     robot_localization_file_path    = os.path.join(pkg_share, 'config/ekf.yaml')
     world_path                      = os.path.join(pkg_share, 'worlds/smalltown.world')  # Ignition uses .sdf
+    robot_description_raw           = xacro.process_file(model_path).toxml()
 
     use_sim_time    = LaunchConfiguration('use_sim_time', default='true')
     use_simulator   = LaunchConfiguration('use_simulator', default='true')
@@ -33,9 +35,8 @@ def generate_launch_description():
         executable   = 'robot_state_publisher',
         parameters   = [{
             'use_sim_time': use_sim_time, 
-            'robot_description': Command(['xacro ', model_path])
+            'robot_description': robot_description_raw
         }],
-        arguments    = [model_path]
     )
 
     # Launch RViz
@@ -55,9 +56,18 @@ def generate_launch_description():
         output       = 'screen'
     )
 
+    # Start joint_state_publisher   (use only when testing limits, servos, etc - but it is a fake node)
+    node_joint_state_publisher = Node(
+        package    = 'joint_state_publisher_gui',
+        executable = 'joint_state_publisher_gui',
+        name       = 'joint_state_publisher_gui',
+        output     = 'screen'
+    )
+
     # Create the launch description and populate
     ld = LaunchDescription([
         node_robot_state_publisher,
+        node_joint_state_publisher,
         node_rqt_robot_steering_node,
         node_rviz,
     ])
